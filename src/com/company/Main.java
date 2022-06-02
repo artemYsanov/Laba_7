9aac2e0ec93e0cd09df4dd08ea62da9ef5491d58
Artem <artem.ysanov2217@gmail.com>
2022-06-02 17:34:18 +0300

    text

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,52 +3,41 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        int x = userInput ("x-");
-        if (x < 0) {
-            System.out.println ("x < 0");
-        } else {
-            int y = userInput ("y-");
-            if (y < 0) {
-                System.out.println ("y < 0");
-            } else {
-                int z = userInput ("z-");
-                if (z < 0) {
-                    System.out.println ("z < 0");
-                } else
-                {
-                    Formula giu = new Formula (x, y, z);
-                    giu.displayFormula ();
-                }
-            }
-        }
+    public static void main(String[] args) throws FormulaException {
+        System.out.println("Введите x:");
+        int x = new Scanner(System.in).nextInt();
+        System.out.println("Введите y:");
+        int y = new Scanner(System.in).nextInt();
+        System.out.println("Введите z:");
+        int z = new Scanner(System.in).nextInt();
+        Formula formm = new Formula(x, y, z);
+        formm.displayFormula(x, y, z);
     }
-    static class Formula {
-        float x;
-        float y;
-        float z;
-        public Formula(float x, float y, float z) {
-            this.x = x;
-            this.y = y;
-            this.z = z;
-        }
-        public void displayFormula() {
-            double l = Math.sqrt ((Math.pow (x, 2) + (Math.pow (y, 2)) + (Math.pow (z, 2))));
-            System.out.println (l);
-        }
+}
+class Formula {
+    float x;
+    float y;
+    float z;
+
+    public Formula(float x, float y, float z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
     }
-    private static int userInput(String message) {
-        Scanner sc = new Scanner (System.in);
-        int i;
-        while (true) {
-            System.out.print (message);
-            try {
-                i = Integer.parseInt (sc.next ());
-                break;
-            } catch (NumberFormatException e) {
-                System.out.println ("Неверный ввод!");
-            }
-        }
-        return i;
+
+    public void displayFormula(int x, int y, int z) throws FormulaException {
+        if (x<0|y<0|z<0) throw new FormulaException("Вы ввели число меньше 0",x);
+        double l = Math.sqrt((Math.pow(x, 2) + (Math.pow(y, 2)) + (Math.pow(z, 2))));
+        System.out.println(l);
+    }
+}
+class FormulaException extends Exception {
+    private float number1;
+    public float getNumber1() {
+        return number1;
+    }
+    public FormulaException(String message, float x) {
+        super(message);
+        number1 = x;
     }
 }
